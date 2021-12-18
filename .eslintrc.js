04991c1181c6c5745ea943a05099841903ce4efe
André Violin <andre.violin@novaandradina.org>
2021-12-18 11:55:40 -0400

    chore: backend backup repository

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    es2020: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 11,
+  },
+  rules: {
+    'no-console': 0,
+    quotes: [2, 'backtick', 'single'],
+    semi: false,
+  },
+}
